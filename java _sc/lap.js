da1e401adba3f0556cd75d0624c38c88310025dc
saif hamaydeh <saif.ahmad12395@gmail.com>
2021-02-23 16:41:06 +0200

    JS addition

---
+++ java _sc/lap.js
@@ -0,0 +1,22 @@
+var age = 30 ; 
+console.log(age);
+var elemnt = document.getElementById('hi')
+
+confirm("welcome in zed shop cars")
+
+var ageUserInput = prompt('enter your age');
+if( ageUserInput >= 16  ){
+    alert("we sugest for you marcedes car");
+    elemnt.innerText='hello in zed shop car';
+
+}
+
+if( ageUserInput < 16  ){
+    alert("we sugest for you buy a bicycle");
+    
+
+}
+
+
+var nameUserInput = prompt('enter your name');
+alert("hello" +"  "+ nameUserInput );
