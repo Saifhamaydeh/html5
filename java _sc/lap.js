70d740a42947fc31136c9a9ac8b124db858ad710
saif hamaydeh <saif.ahmad12395@gmail.com>
2021-02-24 19:45:08 +0200

    loops

--- java _sc/lap.js
+++ java _sc/lap.js
@@ -1,8 +1,10 @@
-var age = 30 ; 
+var age = 3 +'5' ; 
 console.log(age);
 var elemnt = document.getElementById('hi')
 
 confirm("welcome in zed shop cars")
+
+
 
 var ageUserInput = prompt('enter your age');
 if( ageUserInput >= 16  ){
@@ -20,3 +22,44 @@ if( ageUserInput < 16  ){
 
 var nameUserInput = prompt('enter your name');
 alert("hello" +"  "+ nameUserInput );
+ 
+
+ function myFunction() {
+    var x, text;
+  
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("numb").value;
+  
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+
+
+  var a = prompt( "enter your favaret color ")
+  for( var i= 0 ; i < 2 ; i++)
+ {
+   if( a =='red'){
+     document.write("we suggest for you")
+    //  var elemnt = document.getElementById('we')
+    document.write('<img id= "we" src="https://cover.com/blog/wp-content/uploads/2019/03/red-car-insurance-1.jpg" alt="">')
+     
+   }
+   if( a =='blue'){
+    document.write('<img id= "we" src="https://www.autoguide.com/blog/wp-content/uploads/2016/01/Tokyo-Auto-Salon-Dream-04.png" alt="">')
+    var elemnt = document.getElementById('sa')
+    }
+ }
+ var a;
+ var key = "0000";
+ var a = prompt('please enter the key');
+ while (a!= key)
+ {
+  a=prompt('try again');
+ }
+
+
